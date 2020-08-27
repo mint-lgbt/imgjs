a71114a61ef82e940c9acaa6a219eb786d5f9788
Luna <63075513+lunaisnotaboy@users.noreply.github.com>
2020-08-27 15:21:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("This module is not ready for production")
+console.log("There is no command line version of imgjs.\n\nTo see the project, please go to https://github.com/mint-lgbt/imgjs.")
